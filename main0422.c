51d36276e81810cb0503c0c469cc81c3a2fc2db6
zhangmengze <www.201806020429@aliyun.com>
2020-04-22 12:30:33 +0800

    0422 strlen自定义函数的实现

---
+++ main0422.c
@@ -0,0 +1,8 @@
+#include"test.h"
+
+
+int main() {
+	char*str="abcd1234";
+	printf("%d", Mystrlen(str));
+	system("pause");
+}
---
+++ test0422.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+extern int Mystrlen(char* str);
+
+
+#endif
