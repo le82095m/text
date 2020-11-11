9162de4a5d123cf53fc7b098abcba7a4b743cd8d
zhangmengze <www.201806020429@aliyun.com>
2020-11-11 21:41:59 +0800

    main函数

---
+++ Main1111.java
@@ -0,0 +1,12 @@
+package work;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "101011";
+        AddBinary a = new AddBinary();
+        System.out.println(a.addBinary("100", "110010"));
+    }
+}
